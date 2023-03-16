2f0613be9e123f200140c5220240307e7d013c30
Ulyana19 <71721827+Ulyana19@users.noreply.github.com>
2023-03-16 23:34:46 +0300

    .

--- Lesson2/Dialog/src/main/java/ru/mirea/sergeevaum/dialog/MyProgressDialogFragment.java
+++ Lesson2/Dialog/src/main/java/ru/mirea/sergeevaum/dialog/MyProgressDialogFragment.java
@@ -5,8 +5,28 @@ import android.os.Bundle;
 
 import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
 public class MyProgressDialogFragment extends DialogFragment {
-    public ProgressDialog onCreateDialog(Bundle savedInstanceState) {
-        return new ProgressDialog(getActivity());
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("loading")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ((MainActivity)getActivity()).onOkClicked();
+                        dialogInterface.cancel();
+                    }
+                });
+        return builder.create();
     }
 }
